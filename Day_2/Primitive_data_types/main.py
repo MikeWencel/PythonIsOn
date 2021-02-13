f6d36478fcbb4b43a1309d7339e511ec16a1674e
MikeWencel <wencel.michael@gmail.com>
2021-02-13 22:22:46 +0100

    Few changes, going to dataTypes

---
+++ Day_2/Primitive_data_types/main.py
@@ -0,0 +1,26 @@
+#String
+
+#Printing indivudual characters - type index
+print("Hello"[0])
+
+print("123" + "345")
+
+#Integer
+
+print (123 + 456)
+
+
+#Computer ignores underscore, it's good for human (visual)
+print(123_345_544)
+
+#Float (Number with decimal point)
+
+x = 2.443
+print(x)
+
+#Boolean
+
+a = True;
+b = False;
+
+print(a)
