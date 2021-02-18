5ff3b9fd354e0dfcfa9d6a6ca4582420e9225070
MikeWencel <wencel.michael@gmail.com>
2021-02-18 21:25:39 +0100

    You're doing great!

---
+++ Section_first/Day_6/Functions/main.py
@@ -0,0 +1,16 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+
+#def - defining a function
+def my_function():
+    print("hello")
+    print("Bye")
+
+def my_hello(name):
+    print("Hello " + name)
+
+my_function()
+
+my_hello("Mike")
