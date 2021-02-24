c5ef218600d0638903e921dc62bd93e9a59416a5
MikeWencel <wencel.michael@gmail.com>
2021-02-24 22:15:52 +0100

    You're doing great!

--- Section_first/Day_5/for_loop/main.py
+++ Section_first/Day_5/for_loop/main.py
@@ -1,4 +1,15 @@
+import random
+
 fruits = ["Apple","Peach","Ananas"]
 
 for fruit in fruits:
     print(fruit)
+
+numbers_list = []
+number = 0  
+for x in range (1,6):
+    number = random.randint(1,48)
+    numbers_list.append(number)
+
+    
+print(numbers_list)     
